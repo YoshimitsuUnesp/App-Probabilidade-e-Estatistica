1d679733304b8127eeb4b6a1782fa30239111219
YoshimitsuUnesp <yoshimitsu.lanzo@unesp.br>
2023-01-08 23:40:43 -0300

    Atualizando app

--- app/src/main/java/com/example/mestatistica/MainActivity.java
+++ app/src/main/java/com/example/mestatistica/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.mestatistica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private TextView txtEsqueciSenha;
+    private Button btnEntrar;
+    private Button btnNaoPossuiConta;
+    private TextView txtComecarSemLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void leDados(){
+
     }
 }
